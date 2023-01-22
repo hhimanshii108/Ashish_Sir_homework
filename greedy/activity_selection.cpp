97b4c987a9ea0829eab1647bbc7ed27820941f2b
hhimanshii108 <100212269+hhimanshii108@users.noreply.github.com>
2023-01-22 19:57:15 +0530

    Create activity_selection.cpp

---
+++ greedy/activity_selection.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    vector<pair<int,int>>activity;
+    int n,s,e;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>s>>e;
+        activity.push_back(make_pair(e,s));
+    }
+    sort(activity.begin(),activity.end());
+    int count=0;
+    int currEnd=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(activity[i].second > currEnd)
+        {
+            count++;
+                currEnd=activity[i].first;
+        }
+    }
+    cout<<count<<endl;
+}
